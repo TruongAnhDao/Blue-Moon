87c878f0c29fe2835214830276f5bf385c081688
Dao Truong Anh <truongandao16@gmail.com>
2025-12-17 16:22:33 +0700

    upd class nền để code

---
+++ src/main/java/com/bluemoon/app/models/KhoanThuModel.java
@@ -0,0 +1,10 @@
+package com.bluemoon.app.models;
+
+public class KhoanThuModel {
+    private int id;
+    private String tenKhoanThu;
+    private double soTien;
+    private String loaiKhoanThu;
+
+    // Tạo Constructor và Getter/Setter ở đây 
+}
---
+++ src/main/java/com/bluemoon/app/services/KhoanThuService.java
@@ -0,0 +1,8 @@
+package com.bluemoon.app.services;
+
+import java.sql.*;
+import com.bluemoon.app.models.KhoanThuModel;
+
+public class KhoanThuService {
+    // Viết các hàm INSERT, UPDATE vào DB ở đây.
+}
